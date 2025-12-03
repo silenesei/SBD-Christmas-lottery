c7b96e4bf40a1b636c6ff014fde96012698c9422
silenesei <marcelina.klimczewska@gmail.com>
2025-12-03 03:10:20 +0100

    visuals css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 import { useReducer, useEffect, useState } from "react";
 import "./App.css";
+import winterVideo from "./assets/winter_video.mp4";
+import sbdLogo from "./assets/sbd_logo.png";
+import mistletoe from "./assets/mistletoe.png";
+import christmasMusic from "./assets/christmas-music.mp3";
 
 const TOTAL_TICKETS = 200;
 
@@ -16,6 +20,10 @@ export default function App() {
   const [state, dispatch] = useReducer(reducer, { available: [], history: [] });
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
+  const [ticketAlert, setTicketAlert] = useState({
+    message: "",
+    visible: false,
+  });
 
   useEffect(() => {
     const fetchState = async () => {
@@ -31,6 +39,36 @@ export default function App() {
     };
     fetchState();
   }, []);
+
+  useEffect(() => {
+    const player = document.getElementById("carol-player");
+    const playBtn = document.getElementById("play-btn");
+    const pauseBtn = document.getElementById("pause-btn");
+
+    playBtn.addEventListener("click", () => player.play());
+    pauseBtn.addEventListener("click", () => player.pause());
+
+    return () => {
+      playBtn.removeEventListener("click", () => player.play());
+      pauseBtn.removeEventListener("click", () => player.pause());
+    };
+  }, []);
+
+  const showTicketAlert = (name, tickets) => {
+    setTicketAlert({
+      message: (
+        <>
+          <p>
+            Losy dla <b>{name}</b>:
+          </p>
+          <p>
+            <b>{tickets.join(", ")}</b>
+          </p>
+        </>
+      ),
+      visible: true,
+    });
+  };
 
   const buyTickets = async () => {
     if (!name.trim()) {
@@ -59,7 +97,7 @@ export default function App() {
         return;
       }
 
-      alert(`Wylosowano losy dla ${name.trim()}: ${data.tickets.join(", ")}`);
+      showTicketAlert(name.trim(), data.tickets);
       setName("");
       setAmount("");
 
@@ -75,33 +113,82 @@ export default function App() {
   };
 
   return (
-    <div>
-      <h1>Loteria świąteczna</h1>
-      <h2>Stanley Black&Decker</h2>
-      <h3>Wpłata minimalna: 10 zł</h3>
-      <h3>
-        <a href="https://www.ratujemyzwierzaki.pl/sbdlosowaniemikolajki">
-          Link do zbiórki
-        </a>
-      </h3>
+    <div className="main-page">
+      {ticketAlert.visible && (
+        <div
+          className="alert-overlay"
+          onClick={() => setTicketAlert({ message: "", visible: false })}
+        >
+          <div className="custom-alert" onClick={(e) => e.stopPropagation()}>
+            {ticketAlert.message}
+            <button
+              onClick={() => setTicketAlert({ message: "", visible: false })}
+            >
+              ✖
+            </button>
+          </div>
+        </div>
+      )}
+      <video
+        className="background-video"
+        src={winterVideo}
+        loop
+        autoPlay
+        muted
+      ></video>
+      <div id="audio-mini">
+        <button id="play-btn">▶</button>
+        <button id="pause-btn">⏸</button>
+      </div>
+      <audio id="carol-player">
+        <source src={christmasMusic} />
+      </audio>
+      <div className="lottery-form">
+        <img class="header-pic" src={mistletoe} alt="mistletoe-decoration" />
+        <h1 className="title">Loteria świąteczna</h1>
 
-      <input
-        type="text"
-        placeholder="Imię i nazwisko"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Kwota wpłaty"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
+        <h2>
+          Wpłata minimalna: <b>10 PLN</b>
+        </h2>
+        <p>
+          Wszystkie wpłaty zostaną przeznaczone <br /> na rzecz fundacji{" "}
+          <b>"Głosem Zwierząt"</b>.
+        </p>
+        <h3>
+          <a
+            href="https://www.ratujemyzwierzaki.pl/sbdlosowaniemikolajki"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <u>Link do zbiórki</u>
+          </a>
+        </h3>
+        <section className="inputs-btn">
+          <input
+            type="text"
+            placeholder="Imię i nazwisko"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
 
-      <button onClick={buyTickets}>Wygeneruj los</button>
-      <p>
-        Dostępne losy: {state.available.length}/{TOTAL_TICKETS}
-      </p>
+          <input
+            type="text"
+            placeholder="Wpłacona kwota w PLN"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+
+          <button onClick={buyTickets}>LOSUJ</button>
+        </section>
+
+        <p className="available-tickets">
+          Dostępne losy:{" "}
+          <b>
+            {state.available.length}/{TOTAL_TICKETS}
+          </b>
+        </p>
+        <img className="logo-img" src={sbdLogo} alt="logo-image" />
+      </div>
     </div>
   );
 }
